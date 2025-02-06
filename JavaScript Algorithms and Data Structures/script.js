d12931c7efb4078ca05f53f01baa89b48a6a6909
hadirch <hadirich3@gmail.com>
2025-02-06 11:13:07 +0700

    Step 31

--- JavaScript Algorithms and Data Structures/script.js
+++ JavaScript Algorithms and Data Structures/script.js
@@ -5,11 +5,6 @@ let currentWeaponIndex = 0;
 let fighting;
 let monsterHealth;
 let inventory = ["stick"];
-// Step 30
-// Finally, use querySelector() to get the #monsterHealth element. 
-// Because you have already declared a monsterHealth variable earlier, 
-// you need to use a different variable name for this element.
-// Declare a new variable with the const keyword and name it monsterHealthText.
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
@@ -20,3 +15,12 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
+// Step 31
+// In the previous project, you learned how to create a function like this:
+// Example Code
+// function functionName() {
+// }
+// Create an empty function named goStore.
+function goStore(){
+  
+}
