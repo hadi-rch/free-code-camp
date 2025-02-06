cb220263bcf250872c47eba77ef5e7e63473e414
hadirch <hadirich3@gmail.com>
2025-02-06 10:34:29 +0700

    Step 16

--- JavaScript Algorithms and Data Structures/script.js
+++ JavaScript Algorithms and Data Structures/script.js
@@ -5,17 +5,4 @@ let currentWeaponIndex = 0;
 let fighting;
 let monsterHealth;
 let inventory = ["stick"];
-// Step 15
-// JavaScript interacts with the HTML using the Document Object Model, or DOM. 
-// The DOM is a tree of objects that represents the HTML. You can access 
-// the HTML using the document object, which represents your entire HTML document.
-// One method for finding specific elements in your HTML is using 
-// the querySelector() method. The querySelector() method takes a 
-// CSS selector as an argument and returns the first element that 
-// matches that selector. For example, to find the <h1> element in your HTML, you would write:
-// Example Code
-// let h1 = document.querySelector("h1");
-// Note that h1 is a string and matches the CSS selector you would use.
-// Create a button1 variable and use querySelector() to assign it your 
-// element with the id of button1. Remember that CSS id selectors are prefixed with a #.
 let button1 = document.querySelector("#button1")
