e07659aec2fd7a366d80c5bc56ee31a668285119
hadirch <hadirich3@gmail.com>
2025-02-06 10:42:00 +0700

    Step 19

--- JavaScript Algorithms and Data Structures/script.js
+++ JavaScript Algorithms and Data Structures/script.js
@@ -5,11 +5,6 @@ let currentWeaponIndex = 0;
 let fighting;
 let monsterHealth;
 let inventory = ["stick"];
-// Step 18
-// Use querySelector() to get the other two button elements using 
-// their ids: button2 and button3. Store them in variables called 
-// button2 and button3. Remember to use const.
-
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
