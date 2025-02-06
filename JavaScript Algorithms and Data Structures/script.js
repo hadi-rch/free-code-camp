1710e1db446f242dc6c6ded0f72f566cc37b2e9e
hadirch <hadirich3@gmail.com>
2025-02-06 19:06:03 +0700

    Step 95

--- JavaScript Algorithms and Data Structures/script.js
+++ JavaScript Algorithms and Data Structures/script.js
@@ -101,22 +101,24 @@ function buyHealth() {
   }
 }
 
-// Step 93
-// Step 94
-// Add an else statement to your buyWeapon function. In that statement,
-// set text.innerText to equal "You do not have enough gold to buy a weapon.".
+// Step 95
+// Once a player has the best weapon, they cannot buy another one.
+// Wrap all of the code in your buyWeapon function inside another if statement.
+// The condition should check if currentWeaponIndex is less than 3 - the index of the last weapon.
 function buyWeapon() {
-  if (gold >= 30) {
-    gold -= 30;
-    currentWeaponIndex++;
-    goldText.innerText = gold;
-    let newWeapon = weapons[currentWeaponIndex].name;
+  if (currentWeaponIndex < 3) {
+    if (gold >= 30) {
+      gold -= 30;
+      currentWeaponIndex++;
+      goldText.innerText = gold;
+      let newWeapon = weapons[currentWeaponIndex].name;
 
-    text.innerText = "You now have a " + newWeapon + ".";
-    inventory.push(newWeapon);
-    text.innerText += " In your inventory you have: " + inventory;
-  } else {
-    text.innerText = "You do not have enough gold to buy a weapon.";
+      text.innerText = "You now have a " + newWeapon + ".";
+      inventory.push(newWeapon);
+      text.innerText += " In your inventory you have: " + inventory;
+    } else {
+      text.innerText = "You do not have enough gold to buy a weapon.";
+    }
   }
 }
 function fightSlime() {}
