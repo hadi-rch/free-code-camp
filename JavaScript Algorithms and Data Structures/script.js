c3d0026934b4aba67efc37482f8c25d8a50db339
hadirch <hadirich3@gmail.com>
2025-02-06 11:45:34 +0700

    Step 41

--- JavaScript Algorithms and Data Structures/script.js
+++ JavaScript Algorithms and Data Structures/script.js
@@ -21,17 +21,17 @@ button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
 
-// Step 40
-// You will also need to update the functions that run when the buttons are clicked again.
-// In your goStore() function, update the onclick property for each button 
-// to run buyHealth, buyWeapon, and goTown, respectively.
+// Step 41
+// Now you need to modify your display text. Change the innerText 
+// property of the text variable to be "You enter the store.".
 function goStore() {
-  button1.onclick = buyHealth;
-  button2.onclick = buyWeapon;
-  button3.onclick = goTown;
   button1.innerText = "Buy 10 health (10 gold)";
   button2.innerText = "Buy weapon (30 gold)";
   button3.innerText = "Go to town square";
+  button1.onclick = buyHealth;
+  button2.onclick = buyWeapon;
+  button3.onclick = goTown;
+  text.innerText = "You enter the store.";
 }
 function goCave() {
   console.log("Going to cave.");
