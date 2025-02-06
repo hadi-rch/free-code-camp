ad1ec1a40937ef6387f061019d76d14ad5d7603e
hadirch <hadirich3@gmail.com>
2025-02-06 20:24:54 +0700

    Step 119

--- JavaScript Algorithms and Data Structures/script.js
+++ JavaScript Algorithms and Data Structures/script.js
@@ -166,21 +166,16 @@ function fightDragon() {
   fighting = 2;
   goFight();
 }
-// Step 118
-// By default, the HTML element that shows the monster's stats has been hidden with CSS.
-// When the player clicks the "Fight dragon" button, the monster's stats should be displayed.
-// You can accomplish this by using the style and display properties on the monsterStats element.
-// The style property is used to access the inline style of an element and the
-// display property is used to set the visibility of an element.
-// Here is an example of how to update the display for a paragraph element:
-// Example Code
-// const paragraph = document.querySelector('p');
-// paragraph.style.display = 'block';
-// Display the monsterStats element by updating the display property of the style property to block.
+// Step 119
+// Now, you will need to update the text for the current monster's name and health.
+// Start by assigning monsters[fighting].name to the innerText property of monsterName. 
+// Then, assign monsterHealth to the innerText property of monsterHealthText.
 function goFight() {
   update(locations[3]);
   monsterHealth = monsters[fighting].health;
   monsterStats.style.display = "block";
+  monsterName.innerText = monsters[fighting].name;
+  monsterHealthText.innerText = monsterHealth;
 }
 function attack() {}
 function dodge() {}
