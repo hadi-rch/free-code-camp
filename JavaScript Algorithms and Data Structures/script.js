a3771f04b58fe6ae4c31f15d7ce2b9d9b89f0acb
hadirch <hadirich3@gmail.com>
2025-02-06 16:50:25 +0700

    Step 59

--- JavaScript Algorithms and Data Structures/script.js
+++ JavaScript Algorithms and Data Structures/script.js
@@ -16,10 +16,10 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
-// Step 58
-// Add one final property to the object named text. 
-// Give this property the same string value as the one assigned to 
-// text.innerText in the goTown function.
+// Step 59
+// Add a second object to your locations array (remember to separate them with a comma). 
+// Following the pattern you used in the first object, create the same properties 
+// but use the values from the goStore function. Set the name property to store.
 
 const locations = [
   {
@@ -27,6 +27,12 @@ const locations = [
     "button text": ["Go to store","Go to cave","Fight dragon"],
     "button functions": [goStore,goCave,fightDragon],
     text: "You are in the town square. You see a sign that says \"Store\"."
+  },
+  {
+    name: "store",
+    "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
+    "button functions": [buyHealth, buyWeapon, goTown],
+    text: "You enter the store."
   }
 ];
 
