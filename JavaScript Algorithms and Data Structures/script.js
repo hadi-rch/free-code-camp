95e6d5d9cd34256e4acf22ee96f696b3583d07b4
hadirch <hadirich3@gmail.com>
2025-02-06 21:38:02 +0700

    Step 122

--- JavaScript Algorithms and Data Structures/script.js
+++ JavaScript Algorithms and Data Structures/script.js
@@ -174,13 +174,12 @@ function goFight() {
   monsterName.innerText = monsters[fighting].name;
   monsterHealthText.innerText = monsterHealth;
 }
-// Step 121
-// On a new line, use the addition assignment operator(+=), to add the string 
-// " You attack it with your <weapon>." to the text value, replacing <weapon> 
-// with the player's current weapon. Additionally, remember that this line of 
-// text starts with a space so it will properly display.
+// Step 122
+// Next, set health to equal health minus the monster's level. 
+// Remember you can get this from the monsters[fighting].level property.
 function attack() {
   text.innerText = "The " + monsters[fighting].name + " attacks.";
   text.innerText += " You attack it with your "+ weapons[currentWeaponIndex].name + ".";
+  health -= monsters[fighting].level;
 }
 function dodge() {}
