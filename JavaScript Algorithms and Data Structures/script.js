13f65495b17fba23a2ada825d46c29d9b521cc31
hadirch <hadirich3@gmail.com>
2025-02-06 11:09:40 +0700

    Step 29

--- JavaScript Algorithms and Data Structures/script.js
+++ JavaScript Algorithms and Data Structures/script.js
@@ -5,6 +5,17 @@ let currentWeaponIndex = 0;
 let fighting;
 let monsterHealth;
 let inventory = ["stick"];
+// Step 29
+// Just like you did with the buttons, create variables for 
+// the following ids and use querySelector() to give them the element as a value:
+// text, xpText, healthText, goldText, monsterStats, and monsterName.
+// Remember to declare these with the const keyword, and name the variables to match the ids.
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+const monsterStats = document.querySelector("#monsterStats");
+const monsterName = document.querySelector("#monsterName");
