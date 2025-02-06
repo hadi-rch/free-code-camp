82a66d508bc19ddb9d0f15158aac8cf137d05cff
hadirch <hadirich3@gmail.com>
2025-02-06 18:51:38 +0700

    Step 91

--- JavaScript Algorithms and Data Structures/script.js
+++ JavaScript Algorithms and Data Structures/script.js
@@ -101,14 +101,16 @@ function buyHealth() {
   }
 }
 
-// Step 90
-// In the previous project, you learned how to work with the concatenation operator to insert variables into a string like this:
-// Example Code
-// const organization = "freeCodeCamp";
-// // "Hello, our name is freeCodeCamp."
-// "Hello, our name is " + organization + ".";
-// Update the string "You now have a new weapon." to "You now have a " followed by the name of the new weapon, and remember to end the sentence with a period.
+// Step 91
+// Back at the beginning of this project, you created the inventory array. 
+// Add the newWeapon to the end of the inventory array using the push() method.
 
+// In the previous project, you learned how to work with the push method like this:
+
+// Example Code
+// const myArray = [];
+// myArray.push("new item");
+// // myArray is now ["new item"]
 function buyWeapon() {
   if (gold >= 30) {
     gold -= 30;
@@ -117,6 +119,7 @@ function buyWeapon() {
     let newWeapon = weapons[currentWeaponIndex].name;
 
     text.innerText = "You now have a " + newWeapon + ".";
+    inventory.push(newWeapon);
   }
 }
 function fightSlime() {}
