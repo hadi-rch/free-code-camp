a57575ad4b018cd25c1db2e64115d3bd6e9351b6
hadirch <hadirich3@gmail.com>
2025-02-06 18:24:21 +0700

    Step 85

--- JavaScript Algorithms and Data Structures/script.js
+++ JavaScript Algorithms and Data Structures/script.js
@@ -101,17 +101,20 @@ function buyHealth() {
   }
 }
 
-// Step 84
-// The value of the currentWeaponIndex variable corresponds to an index in the weapons array. 
-// The player starts with a "stick", since currentWeaponIndex starts 
-// at 0 and weapons[0] is the "stick" weapon.
-// In the buyWeapon function, use compound assignment to 
-// add 1 to currentWeaponIndex - the user is buying the next weapon in the weapons array.
+// Step 85
+// In the previous project, you learned how to use the increment operator 
+// to increase a variable by 1.
+// Example Code
+// let num = 5;
+// num++;
+// // prints 6
+// console.log(num); 
+// Change your currentWeaponIndex assignment to use the increment operator.
 
 function buyWeapon() {
   if (gold >= 30) {
     gold -= 30;
-    currentWeaponIndex += 1;
+    currentWeaponIndex++;
   }
 }
 function fightSlime() {}
