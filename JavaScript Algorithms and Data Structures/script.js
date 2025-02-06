d0f063ee013ed89dc55aec469609b52947379204
hadirch <hadirich3@gmail.com>
2025-02-06 17:29:22 +0700

    Step 74

--- JavaScript Algorithms and Data Structures/script.js
+++ JavaScript Algorithms and Data Structures/script.js
@@ -70,14 +70,11 @@ function goCave() {
 function fightDragon() {
   console.log("Fighting dragon.");
 }
-
-// Step 73
-// Now that your "store" and "cave" locations are complete, you can code the
-// actions the player takes at those locations. Inside the buyHealth function,
-// set gold equal to gold minus 10.
-// For example, here is how you would set num equal to 5 less than num: num = num - 5;.
+// Step 74
+// After the gold is updated, add a line to set health equal to health plus 10.
 function buyHealth() {
   gold = gold - 10;
+  health  = health + 10;
 }
 
 function buyWeapon() {}
