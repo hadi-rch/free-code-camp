c56713fb75b07203e7ed74190f327b5871118ecf
hadirch <hadirich3@gmail.com>
2025-02-06 20:01:19 +0700

    Step 114

--- JavaScript Algorithms and Data Structures/script.js
+++ JavaScript Algorithms and Data Structures/script.js
@@ -104,7 +104,6 @@ function goStore() {
 function goCave() {
   update(locations[2]);
 }
-function goFight() {}
 
 function buyHealth() {
   if (gold >= 10) {
@@ -166,3 +165,8 @@ function fightDragon() {
   fighting = 2;
   goFight();
 }
+function goFight() {}
+// Step 114
+// At the end of your code, create two empty functions named attack and dodge.
+function attack() {}
+function dodge() {}
