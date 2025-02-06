9ed58eb6fa98175b380e56f1bb158a414ea2c793
hadirch <hadirich3@gmail.com>
2025-02-06 09:36:45 +0700

    Step 5

---
+++ JavaScript Algorithms and Data Structures/script.js
@@ -0,0 +1,5 @@
+// Step 5
+// Your view has been switched to your new script.js file. 
+// Remember that you can use the tabs above to switch between files.
+// Add your console.log("Hello World"); line to this file, and see it appear in your console.
+console.log("Hello World");
