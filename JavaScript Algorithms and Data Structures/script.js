eeea20a52bb68d4c921507b9f2e3168c63415768
hadirch <hadirich3@gmail.com>
2025-02-06 21:44:48 +0700

    Step 124

--- JavaScript Algorithms and Data Structures/script.js
+++ JavaScript Algorithms and Data Structures/script.js
@@ -174,14 +174,19 @@ function goFight() {
   monsterName.innerText = monsters[fighting].name;
   monsterHealthText.innerText = monsterHealth;
 }
-// Step 123
-// Set monsterHealth to monsterHealth minus the power of the player's current weapon.
-// Remember that you can access the power of the player's current weapon 
-// using weapons[currentWeaponIndex].power.
+// Step 124
+// The Math object in JavaScript contains static properties and methods 
+// for mathematical constants and functions. One of those is Math.random(), 
+// which generates a random number from 0 (inclusive) to 1 (exclusive). 
+// Another is Math.floor(), which rounds a given number down to the nearest integer.
+// Using these, you can generate a random number within a range. For example, 
+// this generates a random number between 1 and 5: Math.floor(Math.random() * 5) + 1;.
+// Following this pattern, use the addition operator (+) to add a random number 
+// between 1 and the value of xp to your monsterHealth -= weapons[currentWeaponIndex].power.
 function attack() {
   text.innerText = "The " + monsters[fighting].name + " attacks.";
   text.innerText += " You attack it with your "+ weapons[currentWeaponIndex].name + ".";
   health -= monsters[fighting].level;
-  monsterHealth -=  weapons[currentWeaponIndex].power;
+  monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
 }
 function dodge() {}
