9fe4f4ca9c2bcc4bac29672f3324f6e83f185e82
hadirch <hadirich3@gmail.com>
2025-02-06 17:53:19 +0700

    Step 81

--- JavaScript Algorithms and Data Structures/script.js
+++ JavaScript Algorithms and Data Structures/script.js
@@ -16,9 +16,31 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
-// Step 80
-// Use const to create a weapons variable above your locations array. Assign it an empty array.
-const weapons = [];
+// Step 81
+// Just like your locations array, your weapons array will hold objects.
+// Add four objects to the weapons array, each with two properties: name and power.
+// The first should have the name set to "stick" and the power set to 5.
+// The second should be "dagger" and 30. The third, "claw hammer" and 50.
+// The fourth, "sword" and 100.
+
+const weapons = [
+  {
+    name: "stick",
+    power: 5,
+  },
+  {
+    name: "dagger",
+    power: 30,
+  },
+  {
+    name: "claw hammer",
+    power: 50,
+  },
+  {
+    name: "sword",
+    power: 100,
+  },
+];
 
 const locations = [
   {
