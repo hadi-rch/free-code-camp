9418e700ded87da7a994288bf91f1f308334bd50
hadirch <hadirich3@gmail.com>
2025-02-06 21:39:22 +0700

    Step 123

--- JavaScript Algorithms and Data Structures/script.js
+++ JavaScript Algorithms and Data Structures/script.js
@@ -174,12 +174,14 @@ function goFight() {
   monsterName.innerText = monsters[fighting].name;
   monsterHealthText.innerText = monsterHealth;
 }
-// Step 122
-// Next, set health to equal health minus the monster's level. 
-// Remember you can get this from the monsters[fighting].level property.
+// Step 123
+// Set monsterHealth to monsterHealth minus the power of the player's current weapon.
+// Remember that you can access the power of the player's current weapon 
+// using weapons[currentWeaponIndex].power.
 function attack() {
   text.innerText = "The " + monsters[fighting].name + " attacks.";
   text.innerText += " You attack it with your "+ weapons[currentWeaponIndex].name + ".";
   health -= monsters[fighting].level;
+  monsterHealth -=  weapons[currentWeaponIndex].power;
 }
 function dodge() {}
