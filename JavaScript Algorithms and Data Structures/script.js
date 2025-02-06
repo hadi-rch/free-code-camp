5e59c206f395439b7958db077b53b54c5d4453eb
hadirch <hadirich3@gmail.com>
2025-02-06 11:29:31 +0700

    Step 37

--- JavaScript Algorithms and Data Structures/script.js
+++ JavaScript Algorithms and Data Structures/script.js
@@ -15,24 +15,16 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
-// Step 36
-// button1 represents your first button element. These elements have a 
-// special property called onclick, which you can use to determine what 
-// happens when someone clicks that button.
-// You can access properties in JavaScript a couple of different ways. 
-// The first is with dot notation. Here is an example of using dot 
-// notation to set the onclick property of a button to a function reference.
-// Example Code
-// button.onclick = myFunction;
-// In this example, button is the button element, and myFunction is a reference to a function. 
-// When the button is clicked, myFunction will be called.
-// Use dot notation to set the onclick property of your button1 to the 
-// function reference of goStore. Note that button1 is already declared, 
-// so you don't need to use let or const.
-
+// Step 37
+// Using the same syntax, set the onclick properties of button2 
+// and button3 to goCave and fightDragon respectively.
+// Once you have done that, open your console and 
+// try clicking the buttons on your project.
 
 // initialize buttons.
 button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
 function goStore() {
   console.log("Going to store.");
 }
