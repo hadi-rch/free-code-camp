171ffa97d578c478c8d1fbb68470974c0009c81b
hadirch <hadirich3@gmail.com>
2025-02-06 09:50:17 +0700

    Step 9

--- JavaScript Algorithms and Data Structures/script.js
+++ JavaScript Algorithms and Data Structures/script.js
@@ -1,6 +1,11 @@
-// Step 8
-// Create another variable called currentWeaponIndex and set it to 0.
+// Step 10
+// Declare two more variables named monsterHealth and inventory.
+// For your inventory variable, assign it the value of an array containing the string "stick".
+// Remember that you worked with arrays in the previous project like this:
+// Example Code
+// let exampleArray = ["first", "second", "third"];
 let xp = 0;
 let health= 100;
 let gold= 50;
 let currentWeaponIndex = 0;
+let fighting;
