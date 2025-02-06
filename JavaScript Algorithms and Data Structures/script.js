c5cd4af3e3b4cdffa06c91ac1b4539adec36593c
hadirch <hadirich3@gmail.com>
2025-02-06 19:28:53 +0700

    Step 104

--- JavaScript Algorithms and Data Structures/script.js
+++ JavaScript Algorithms and Data Structures/script.js
@@ -121,12 +121,13 @@ function buyWeapon() {
     button2.onclick = sellWeapon;
   }
 }
-// Step 103
-// Players should not be able to sell their only weapon. Inside the sellWeapon function,
-// add an if statement with a condition that checks if the length of the inventory
-// array is greater than 1.
+// Step 104
+// Inside the if statement, set gold equal to 15 more than its current value. 
+// Also update goldText.innerText to the new value.
 function sellWeapon() {
   if (inventory.length > 1) {
+    gold += 15;
+    goldText.innerText = gold;
   }
 }
 function fightSlime() {}
