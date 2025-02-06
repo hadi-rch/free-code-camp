775b169c4f0472111ec2620e30cf6ebf435e677e
hadirch <hadirich3@gmail.com>
2025-02-06 18:38:16 +0700

    Step 90

--- JavaScript Algorithms and Data Structures/script.js
+++ JavaScript Algorithms and Data Structures/script.js
@@ -101,9 +101,13 @@ function buyHealth() {
   }
 }
 
-// Step 89
-// weapons[currentWeaponIndex] is an object. Use dot notation to get the name 
-// property of that object.
+// Step 90
+// In the previous project, you learned how to work with the concatenation operator to insert variables into a string like this:
+// Example Code
+// const organization = "freeCodeCamp";
+// // "Hello, our name is freeCodeCamp."
+// "Hello, our name is " + organization + ".";
+// Update the string "You now have a new weapon." to "You now have a " followed by the name of the new weapon, and remember to end the sentence with a period.
 
 function buyWeapon() {
   if (gold >= 30) {
@@ -112,7 +116,7 @@ function buyWeapon() {
     goldText.innerText = gold;
     let newWeapon = weapons[currentWeaponIndex].name;
 
-    text.innerText = "You now have a new weapon.";
+    text.innerText = "You now have a " + newWeapon + ".";
   }
 }
 function fightSlime() {}
