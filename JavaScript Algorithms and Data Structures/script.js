0107bf25a0afbaaaebb626132b092ed60184d247
hadirch <hadirich3@gmail.com>
2025-02-06 17:07:14 +0700

    Step 67

--- JavaScript Algorithms and Data Structures/script.js
+++ JavaScript Algorithms and Data Structures/script.js
@@ -40,17 +40,18 @@ button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
 
-// Step 66
-// Now update button2.innerText and button3.innerText to be assigned 
-// the second and third values of the "button text" array, respectively.
+// Step 67
+// Following the same pattern as you did for the button text, update 
+// the three buttons' onclick assignments to be the first, second, 
+// and third values of the "button functions" array.
 
 function update(location) {
   button1.innerText = location["button text"][0];
   button2.innerText = location["button text"][1];
   button3.innerText = location["button text"][2];
-  button1.onclick = goStore;
-  button2.onclick = goCave;
-  button3.onclick = fightDragon;
+  button1.onclick = location["button functions"][0];
+  button2.onclick = location["button functions"][1];
+  button3.onclick = location["button functions"][2];
   text.innerText =
     'You are in the town square. You see a sign that says "Store".';
 }
