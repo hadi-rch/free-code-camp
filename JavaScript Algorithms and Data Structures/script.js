19355c4b3d9872ed92f0aecd50ffec63d04009f1
hadirch <hadirich3@gmail.com>
2025-02-06 13:50:25 +0700

    Step 48

--- JavaScript Algorithms and Data Structures/script.js
+++ JavaScript Algorithms and Data Structures/script.js
@@ -16,14 +16,22 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
-// Step 47
-// In your role playing game, you will be able to visit different locations like the store, 
-// the cave, and the town square. You will need to create a data structure that will 
-// hold the different locations.
-// Use const to create a variable called locations and assign it an empty array.
-
 const locations  = [];
 
+// Step 48
+// Before you can begin to build out your locations array, you will first need to learn about objects. Objects are an important data type in JavaScript. The next few steps will be dedicated to learning about them so you will better understand how to apply them in your project.
+// Objects are non primitive data types that store key-value pairs. Non primitive data types are mutable data types that are not undefined, null, boolean, number, string, or symbol. Mutable means that the data can be changed after it is created.
+// Here is the basic syntax for an object:
+// Example Code
+// {
+//   key: value
+// }
+// You will learn about keys and values in the next few steps.
+// For now, create a const variable called cat and assign it an empty object {}.
+// Below that cat variable, add a console.log(cat) statement to see the object in the console.
+
+const cat = {};
+console.log(cat);
 
 // initialize buttons.
 button1.onclick = goStore;
