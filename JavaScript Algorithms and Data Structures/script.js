8ae88cce6e38c740b41505c53f8b67538ce7c824
hadirch <hadirich3@gmail.com>
2025-02-06 19:30:52 +0700

    Step 105

--- JavaScript Algorithms and Data Structures/script.js
+++ JavaScript Algorithms and Data Structures/script.js
@@ -121,10 +121,18 @@ function buyWeapon() {
     button2.onclick = sellWeapon;
   }
 }
-// Step 104
-// Inside the if statement, set gold equal to 15 more than its current value. 
-// Also update goldText.innerText to the new value.
+// Step 105
+// The next step is to create a variable called currentWeapon.
+// Example Code
+// let num = 1;
+// if (num === 1) {
+//   let num = 2; // this num is scoped to the if statement
+//   console.log(num); // expected output: 2
+// }
+// console.log(num); // expected output: 1 (the global variable)
+// Use the let keyword to create a variable named currentWeapon. Don't assign it a value yet.
 function sellWeapon() {
+  let currentWeapon;
   if (inventory.length > 1) {
     gold += 15;
     goldText.innerText = gold;
