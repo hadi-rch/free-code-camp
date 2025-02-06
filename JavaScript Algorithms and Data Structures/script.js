ab81a73505aff7c402b81a64023a388d4b30f9ba
hadirch <hadirich3@gmail.com>
2025-02-06 17:09:37 +0700

    Step 68

--- JavaScript Algorithms and Data Structures/script.js
+++ JavaScript Algorithms and Data Structures/script.js
@@ -40,10 +40,12 @@ button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
 
-// Step 67
-// Following the same pattern as you did for the button text, update 
-// the three buttons' onclick assignments to be the first, second, 
-// and third values of the "button functions" array.
+// Step 68
+// Finally, update the text.innerText assignment to equal the text from the 
+// location object. However, instead of using bracket notation, use dot notation.
+// Here is an example of accessing the name property of an object called person:
+// Example Code
+// person.name
 
 function update(location) {
   button1.innerText = location["button text"][0];
@@ -52,8 +54,7 @@ function update(location) {
   button1.onclick = location["button functions"][0];
   button2.onclick = location["button functions"][1];
   button3.onclick = location["button functions"][2];
-  text.innerText =
-    'You are in the town square. You see a sign that says "Store".';
+  text.innerText = location.text;
 }
 
 function goTown() {
