f254d524b30d859c2b58ddc317eb2d46cdde533e
hadirch <hadirich3@gmail.com>
2025-02-06 22:04:17 +0700

    Step 131

--- JavaScript Algorithms and Data Structures/script.js
+++ JavaScript Algorithms and Data Structures/script.js
@@ -188,15 +188,16 @@ function attack() {
     defeatMonster();
   }
 }
-// Step 130
-// In your defeatMonster function, set gold equal to gold plus the monster's level times 6.7. Remember you can get the monster's level by using monsters[fighting].level.
-// Here is an example of setting num to num plus 5 * 8: num += 5 * 8. Use Math.floor() to round the result down.
+
 function dodge() {
   text.innerText = "You dodge the attack from the " + monsters[fighting].name;
 
 }
+// Step 131
+// Set xp to xp plus the monster's level.
 function defeatMonster () {
  gold += Math.floor(monsters[fighting].level * 6.7);
+ xp += monsters[fighting].level;
 
 }
 function lose () {
