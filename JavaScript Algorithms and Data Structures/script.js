14fa6dba4987abbe77b31c715158250d9b97c2a1
hadirch <hadirich3@gmail.com>
2025-02-06 14:10:51 +0700

    Step 55

--- JavaScript Algorithms and Data Structures/script.js
+++ JavaScript Algorithms and Data Structures/script.js
@@ -16,18 +16,19 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
-// Step 54
-// Your locations array will hold different locations like the store, the cave, and the town square. Each location will be represented as an object.
-// Inside your locations array, add an object. Inside that object add a key called name with a value of "town square".
-// Remember to follow this syntax:
+// Step 55
+// Just like array values, object properties are separated by a comma. Add a comma after your name property and add a button text property with the value of an empty array.
+// Since the property name has a space in it, you will need to surround it with quotes.
 // Example Code
 // {
-//   key: value
+//   name: "Naomi",
+//   "favorite color": "purple"
 // }
 
 const locations = [
   {
-    name: "town square"
+    name: "town square",
+    "button text": []
   }
 ];
 
