0814967685602d5db7c416aec2a6919b2297100d
hadirch <hadirich3@gmail.com>
2025-02-06 09:39:56 +0700

    Step 7

--- JavaScript Algorithms and Data Structures/script.js
+++ JavaScript Algorithms and Data Structures/script.js
@@ -1,10 +1,6 @@
-// Step 6
-// Remove your console.log("Hello World"); line to begin writing your project code.
-// In the previous project, you learned how to work with variables and the let keyword like this:
-// Example Code
-// let camperbot;
-// You also learned how to initialize a variable with a value like this:
-// Example Code
-// let age = 32;
-// Use the let keyword to declare a variable called xp and assign it the value of 0, a number.
+// Step 7
+// Initialize another variable called health with a value of 100, 
+// and a variable called gold with a value of 50.
 let xp = 0;
+let health= 100;
+let gold= 50;
