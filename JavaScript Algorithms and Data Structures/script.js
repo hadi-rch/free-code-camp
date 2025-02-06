c65033ab418e00405a80ff102e35c6687d56e1a1
hadirch <hadirich3@gmail.com>
2025-02-06 17:45:41 +0700

    Step 78

--- JavaScript Algorithms and Data Structures/script.js
+++ JavaScript Algorithms and Data Structures/script.js
@@ -70,21 +70,23 @@ function goCave() {
 function fightDragon() {
   console.log("Fighting dragon.");
 }
-// Step 77
-// What if the player doesn't have enough gold to buy health? You should use an if statement to check if the player has enough gold to buy health.
-// In the previous project, you learned how to work with if statements like this:
+// Step 78
+// Now when a player tries to buy health, it will only work if they have enough money. 
+// If they do not, nothing will happen. Add an else statement where you can put code 
+// to run if a player does not have enough money.
+// In the previous project, you learned how to work with else statements like this:
 // Example Code
-// const num = 5;
-// if (num >= 3) {
-//   console.log("This code will run because num is greater than or equal to 3.");
+// if (num >= 5) {
+// } else {
 // }
-// Start by placing all of the code in your buyHealth function inside an if statement. For the if statement condition, check if gold is greater than or equal to 10.
 function buyHealth() {
   if (gold >= 10) {
     gold -= 10;
     health += 10;
     goldText.innerText = gold;
     healthText.innerText = health;
+  } else {
+    
   }
 }
 
