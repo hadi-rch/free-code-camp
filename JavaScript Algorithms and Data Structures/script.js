f6e43b5da1747b5de1ff777ba484459e23883876
hadirch <hadirich3@gmail.com>
2025-02-06 16:43:42 +0700

    Step 57

--- JavaScript Algorithms and Data Structures/script.js
+++ JavaScript Algorithms and Data Structures/script.js
@@ -16,15 +16,16 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
-// Step 56
-// Give your empty button text array three string elements. 
-// Use the three strings being assigned to the button innerText 
-// properties in the goTown function. Remember that array values are separated by commas.
-
+// Step 57
+// Create another property in your object called button functions. 
+// Give this property an array containing the three functions assigned 
+// to the onclick properties in the goTown function. Remember that these 
+// functions are variables, not strings, and should not be wrapped in quotes.
 const locations = [
   {
     name: "town square",
-    "button text": ["Go to store","Go to cave","Fight dragon"]
+    "button text": ["Go to store","Go to cave","Fight dragon"],
+    "button functions": [goStore,goCave,fightDragon]
   }
 ];
 
