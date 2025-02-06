28361d358c5f39a0aebd563ab51823601e971a76
hadirch <hadirich3@gmail.com>
2025-02-06 09:38:10 +0700

    Step 6

--- JavaScript Algorithms and Data Structures/script.js
+++ JavaScript Algorithms and Data Structures/script.js
@@ -1,5 +1,10 @@
-// Step 5
-// Your view has been switched to your new script.js file. 
-// Remember that you can use the tabs above to switch between files.
-// Add your console.log("Hello World"); line to this file, and see it appear in your console.
-console.log("Hello World");
+// Step 6
+// Remove your console.log("Hello World"); line to begin writing your project code.
+// In the previous project, you learned how to work with variables and the let keyword like this:
+// Example Code
+// let camperbot;
+// You also learned how to initialize a variable with a value like this:
+// Example Code
+// let age = 32;
+// Use the let keyword to declare a variable called xp and assign it the value of 0, a number.
+let xp = 0;
