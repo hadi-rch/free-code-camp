4b740a5d25b5781f419fc9a07c3bc20edcf21dca
hadirch <hadirich3@gmail.com>
2025-02-06 17:03:12 +0700

    Step 65

--- JavaScript Algorithms and Data Structures/script.js
+++ JavaScript Algorithms and Data Structures/script.js
@@ -40,14 +40,13 @@ button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
 
-// Step 64
-// Now your update function needs to use the argument you pass into it.
-// Inside the update function, change the value of the button1.innerText 
-// assignment to be location["button text"]. That way, you use bracket notation 
-// to get the "button text" property of the location object passed into the function.
+// Step 65
+// location["button text"] is an array with three elements. 
+// Change the button1.innerText assignment to be location["button text"][0] 
+// which represents the first element of the array.
 
 function update(location) {
-  button1.innerText = location["button text"];
+  button1.innerText = location["button text"][0];
   button2.innerText = "Go to cave";
   button3.innerText = "Fight dragon";
   button1.onclick = goStore;
