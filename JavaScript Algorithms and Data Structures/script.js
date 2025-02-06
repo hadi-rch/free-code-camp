c765c5cb93265d08e2b2e4ebbbbb057bfd1c8987
hadirch <hadirich3@gmail.com>
2025-02-06 10:09:59 +0700

    Step 11

--- JavaScript Algorithms and Data Structures/script.js
+++ JavaScript Algorithms and Data Structures/script.js
@@ -1,9 +1,3 @@
-// Step 10
-// Declare two more variables named monsterHealth and inventory.
-// For your inventory variable, assign it the value of an array containing the string "stick".
-// Remember that you worked with arrays in the previous project like this:
-// Example Code
-// let exampleArray = ["first", "second", "third"];
 let xp = 0;
 let health= 100;
 let gold= 50;
