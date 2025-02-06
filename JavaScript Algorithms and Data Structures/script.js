dc8f805d610688cd1c9f54b256baba4cb960cec1
hadirch <hadirich3@gmail.com>
2025-02-06 10:36:43 +0700

    Step 17

--- JavaScript Algorithms and Data Structures/script.js
+++ JavaScript Algorithms and Data Structures/script.js
@@ -5,4 +5,11 @@ let currentWeaponIndex = 0;
 let fighting;
 let monsterHealth;
 let inventory = ["stick"];
-let button1 = document.querySelector("#button1")
+// Step 17
+// button1 is a variable that is not going to be reassigned. 
+// If you are not going to assign a new value to a variable, 
+// it is best practice to use the const keyword to declare it instead 
+// of the let keyword. This will tell JavaScript to throw an error if you accidentally reassign it.
+// Change your button1 variable to be declared with the const keyword.
+
+const button1 = document.querySelector("#button1")
