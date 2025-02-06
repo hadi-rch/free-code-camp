d021d05079d1a572cc8b96ad92b938268b3c2895
hadirch <hadirich3@gmail.com>
2025-02-06 19:21:59 +0700

    Step 101

--- JavaScript Algorithms and Data Structures/script.js
+++ JavaScript Algorithms and Data Structures/script.js
@@ -1,11 +1,5 @@
 let xp = 0;
 let health = 100;
-
-// Step 100
-// Now that you are finished testing that portion of the buyWeapon function, 
-// you can set your gold variable back to 50.
-// Note: The HTML has already been updated to reflect the original value of gold.
-
 let gold = 50;
 let currentWeaponIndex = 0;
 let fighting;
@@ -107,7 +101,11 @@ function buyHealth() {
   }
 }
 
-
+// Step 101
+// Once a player has the most powerful weapon, you can give 
+// them the ability to sell their old weapons.
+// In the outer else statement, set button2.innerText to "Sell weapon for 15 gold". 
+// Also set button2.onclick to the function name sellWeapon.
 
 function buyWeapon() {
   if (currentWeaponIndex < weapons.length - 1) {
@@ -125,6 +123,8 @@ function buyWeapon() {
     }
   } else {
     text.innerText = "You already have the most powerful weapon!";
+    button2.innerText = "Sell weapon for 15 gold";
+    button2.onclick = sellWeapon;
   }
 }
 function fightSlime() {}
