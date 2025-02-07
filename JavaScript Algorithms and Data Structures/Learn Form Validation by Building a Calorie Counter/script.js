3f418ea585d1221b80d73c4ec8fbc87ab14ebd32
hadirch <hadirich3@gmail.com>
2025-02-07 19:11:34 +0700

    Step 34

--- JavaScript Algorithms and Data Structures/Learn Form Validation by Building a Calorie Counter/script.js
+++ JavaScript Algorithms and Data Structures/Learn Form Validation by Building a Calorie Counter/script.js
@@ -12,10 +12,17 @@ function cleanInputString(str) {
 }
 
 /*
-Step 33
-There is a shorthand character class to match any digit: \d. Replace your [0-9] character classes with this shorthand.
+Step 34
+Strings have a .match() method, which takes a regex argument. .match() will return an array of match results – containing either the first match, or all matches if the global flag is used.
+
+Example Code
+const str = 'example string';
+const regex = /example/;
+const result = str.match(regex); // Returns ['example']
+Return the result of calling the .match() method on str and passing your regex variable as the argument. You'll use this match result later on.
 
 */
 function isInvalidInput(str) {
-    const regex = /\d+e\d+/i;
+  const regex = /\d+e\d+/i;
+  return str.match(regex);
 }
