4f5ebad6eea9693bbd0f42eb409fddf3485ec6db
hadirch <hadirich3@gmail.com>
2025-02-07 17:43:28 +0700

    Step 16

--- JavaScript Algorithms and Data Structures/Learn Form Validation by Building a Calorie Counter/script.js
+++ JavaScript Algorithms and Data Structures/Learn Form Validation by Building a Calorie Counter/script.js
@@ -1,8 +1,11 @@
 /*
-Step 15
-Get your #budget element and assign it to budgetNumberInput, 
-and your #entry-dropdown element and assign it to entryDropdown.
+Step 16
+Following the same pattern, assign your #add-entry element to addEntryButton, 
+your #clear element to clearButton, and your #output element to output.
 */
 const calorieCounter = document.getElementById('calorie-counter');
 const budgetNumberInput = document.getElementById('budget');
 const entryDropdown = document.getElementById('entry-dropdown');
+const addEntryButton = document.getElementById('add-entry');
+const clearButton = document.getElementById('clear');
+const output = document.getElementById('output');
