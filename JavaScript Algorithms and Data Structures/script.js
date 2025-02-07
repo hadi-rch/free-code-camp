d959662b8ab33337ba9488cb849bf2c57243c1be
hadirch <hadirich3@gmail.com>
2025-02-07 14:17:50 +0700

    Step 159

--- JavaScript Algorithms and Data Structures/script.js
+++ JavaScript Algorithms and Data Structures/script.js
@@ -265,10 +265,13 @@ function restart() {
   goTown();
 }
 
-// Step 158
-// Now you can add a small easter egg (hidden feature) to your game.
-
-// Create a new function called easterEgg which calls the update function with locations[7] as the argument.
 function easterEgg () {
   update(locations[7]);
+}
+
+// Step 159
+// Create an empty pick function with a parameter named guess.
+
+function pick (guess) {
+
 }
