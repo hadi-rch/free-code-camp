21c719e25146ce9856ebfa7686cf65c03a0a7906
hadirch <hadirich3@gmail.com>
2025-02-07 14:26:07 +0700

    Step 163

--- JavaScript Algorithms and Data Structures/script.js
+++ JavaScript Algorithms and Data Structures/script.js
@@ -283,9 +283,17 @@ function pickEight() {
   pick(8);
 }
 
-// Step 162
-// Inside pick, use const to initialize a variable named numbers and set it to an empty array.
+// Step 163
+// After your numbers array, create a while loop that runs as long as numbers.length is less than 10.
+
+// In the previous project, you learned how to work with while loops like this:
+
+// Example Code
+// while (condition) {
+//   // code to run
+// }
 
 function pick(guess) {
  const numbers = [];
+ while (numbers.length < 10){}
 }
