9368506a74d3f1088469465ad4bca0b7e3aec805
hadirch <hadirich3@gmail.com>
2025-02-07 14:48:14 +0700

    Step 170

--- JavaScript Algorithms and Data Structures/script.js
+++ JavaScript Algorithms and Data Structures/script.js
@@ -104,11 +104,11 @@ const locations = [
     "button functions": [restart, restart, restart],
     text: "You defeat the dragon! YOU WIN THE GAME! &#x1F389;",
   },
-  { 
-    name: "easter egg", 
-    "button text": ["2", "8", "Go to town square?"], 
-    "button functions": [ pickTwo, pickEight, goTown], 
-    text: "You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!"
+  {
+    name: "easter egg",
+    "button text": ["2", "8", "Go to town square?"],
+    "button functions": [pickTwo, pickEight, goTown],
+    text: "You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!",
   },
 ];
 
@@ -295,15 +295,17 @@ function pickEight() {
 // After your for loop, add an if statement to check if the guess is in the numbers array. You can use the .includes() method to check if the array contains the guess.
 
 function pick(guess) {
- const numbers = [];
- while (numbers.length < 10){
-  numbers.push(Math.floor(Math.random() * 11));
- }
- text.innerText = "You picked " + guess + ". Here are the random numbers:\n" ;
- for(let i = 0; i < 10; i++){
-  text.innerText += numbers[i] + "\n";
- }
- if(numbers.includes(guess)){
-
- }
+  const numbers = [];
+  while (numbers.length < 10) {
+    numbers.push(Math.floor(Math.random() * 11));
+  }
+  text.innerText = "You picked " + guess + ". Here are the random numbers:\n";
+  for (let i = 0; i < 10; i++) {
+    text.innerText += numbers[i] + "\n";
+  }
+  if (numbers.includes(guess)) {
+    text.innerText += "Right! You win 20 gold!";
+    gold += 20;
+    goldText.innerText = gold;
+  }
 }
