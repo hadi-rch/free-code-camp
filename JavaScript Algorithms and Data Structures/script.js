d1a10f79f13af8f3dd6ed8d70ccfd9b259300ebd
hadirch <hadirich3@gmail.com>
2025-02-07 13:51:37 +0700

    Step 150

--- JavaScript Algorithms and Data Structures/script.js
+++ JavaScript Algorithms and Data Structures/script.js
@@ -198,13 +198,16 @@ function goFight() {
   monsterHealthText.innerText = monsterHealth;
 }
 
+// Step 150 In your attack function, find the line of code that updates the monsterHealth variable and place it within an if block with a condition that calls the isMonsterHit function.
+
 function attack() {
   text.innerText = "The " + monsters[fighting].name + " attacks.";
   text.innerText +=
     " You attack it with your " + weapons[currentWeaponIndex].name + ".";
   health -= getMonsterAttackValue(monsters[fighting].level);
-  monsterHealth -=
-    weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
+  if (isMonsterHit()) { 
+    monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
+    }
   healthText.innerText = health;
   monsterHealthText.innerText = monsterHealth;
   if (health <= 0) {
@@ -217,20 +220,6 @@ function attack() {
     }
   }
 }
-
-// Step 149
-// If you play the game in its current state you might notice a bug. If your xp is high enough, the getMonsterAttackValue function will return a negative number, which will actually add to your total health when fighting a monster! You can fix this issue by using a ternary operator to ensure negative values are not returned.
-// The ternary operator is a conditional operator and can be used as a one-line if-else statement. The syntax is: condition ? expressionIfTrue : expressionIfFalse.
-// Here is an example of returning a value using an if-else statement and a refactored example using a ternary operator:
-// Example Code
-// // if-else statement
-// if (score > 0) {
-//   return score
-// } else {
-//   return default_score
-// }
-// // ternary operator
-// return score > 0 ? score : default_score
 
 function getMonsterAttackValue(level) {
   const hit = (level * 5) - (Math.floor(Math.random() * xp));
