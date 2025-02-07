d0ba3589378222c01b0bf0fa776e50900fe1cf4d
hadirch <hadirich3@gmail.com>
2025-02-07 19:29:38 +0700

    Step 40

--- JavaScript Algorithms and Data Structures/Learn Form Validation by Building a Calorie Counter/script.js
+++ JavaScript Algorithms and Data Structures/Learn Form Validation by Building a Calorie Counter/script.js
@@ -12,16 +12,16 @@ function cleanInputString(str) {
 }
 
 /*
-Step 39
-Now that you have finished testing the value of entryDropdown.value, you can remove the console.log statement.
-Your next step is to allow users to add entries to the calorie counter. Declare an empty function addEntry. This function should not take any parameters.
+Step 40
+You'll need to know which category the entry goes in. Thankfully, you added a dropdown for the user to select a category.
+Remember that you queried that dropdown earlier in your JavaScript and assigned it to the entryDropdown variable. You can use the value property to get the value of the selected option.
+Use concatenation to add a # to the beginning of the value property of entryDropdown, and assign that result to a targetId variable.
 
 */
 function isInvalidInput(str) {
   const regex = /\d+e\d+/i;
   return str.match(regex);
 }
-function addEntry (){
-  
+function addEntry() {
+  let targetId = "#" + entryDropdown.value;
 }
-
