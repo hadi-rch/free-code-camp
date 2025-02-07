050c0515165fdc6f176dd1c7ae7885f5fd4a5f31
hadirch <hadirich3@gmail.com>
2025-02-07 19:23:29 +0700

    Step 39

--- JavaScript Algorithms and Data Structures/Learn Form Validation by Building a Calorie Counter/script.js
+++ JavaScript Algorithms and Data Structures/Learn Form Validation by Building a Calorie Counter/script.js
@@ -12,16 +12,16 @@ function cleanInputString(str) {
 }
 
 /*
-Step 38
-Now you need to retrieve the value of entryDropdown.value to get the currently selected option from the dropdown.
-Print entryDropdown.value to the console to see its value.
-Since entryDropdown.value is in a static context (outside of an event listener), it only shows the value at the moment the code runs. This means it won't automatically update as the user interacts with the dropdown.
-It will capture the initial value (in this case, "breakfast") and won't reflect any changes the user makes afterward.
+Step 39
+Now that you have finished testing the value of entryDropdown.value, you can remove the console.log statement.
+Your next step is to allow users to add entries to the calorie counter. Declare an empty function addEntry. This function should not take any parameters.
 
 */
 function isInvalidInput(str) {
   const regex = /\d+e\d+/i;
   return str.match(regex);
 }
-console.log(entryDropdown.value);
+function addEntry (){
+  
+}
 
