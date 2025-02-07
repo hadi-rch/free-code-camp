32f02ef8e9f71e6d86f9c4fe3b8c02174e5729a6
hadirch <hadirich3@gmail.com>
2025-02-07 14:31:13 +0700

    Step 165

--- JavaScript Algorithms and Data Structures/script.js
+++ JavaScript Algorithms and Data Structures/script.js
@@ -283,12 +283,13 @@ function pickEight() {
   pick(8);
 }
 
-// Step 164
-// Inside your while loop, push a random number between 0 and 10 to the end of the numbers array. You can create this random number with Math.floor(Math.random() * 11).
+// Step 165
+// After the while loop, set text.innerText to equal "You picked <someGuess>. Here are the random numbers:". Replace <someGuess> with the guess function parameter.
 
 function pick(guess) {
  const numbers = [];
  while (numbers.length < 10){
   numbers.push(Math.floor(Math.random() * 11));
  }
+ text.innerText = "You picked " + guess + ". Here are the random numbers:" ;
 }
