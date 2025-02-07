6cb77d86ee87f93b2889626f7d37d9921e59bd91
hadirch <hadirich3@gmail.com>
2025-02-07 17:37:38 +0700

    Step 14

---
+++ JavaScript Algorithms and Data Structures/Learn Form Validation by Building a Calorie Counter/script.js
@@ -0,0 +1,9 @@
+/*Step 14
+It is time to start writing the script that makes your form work.
+To access an HTML element with a given id name, you can use the getElementById() method. Here's an example of how to use this method:
+Example Code
+<h1 id="title">Main title</h1>
+Example Code
+const mainTitleElement = document.getElementById('title');
+Begin by getting the form element (using the id) and storing it in a variable called calorieCounter.*/
+const calorieCounter = document.getElementById('calorie-counter');
