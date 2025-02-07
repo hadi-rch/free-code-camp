11166ca1ad2647517a704923b36a021e45eb34ef
hadirch <hadirich3@gmail.com>
2025-02-07 15:43:23 +0700

    Step 3

--- JavaScript Algorithms and Data Structures/Learn Basic Debugging by Building a Random Background Color Changer/script.js
+++ JavaScript Algorithms and Data Structures/Learn Basic Debugging by Building a Random Background Color Changer/script.js
@@ -1,9 +1,13 @@
-// Step 2
-// Now, CamperBot is trying to create a function that will return a random index from the darkColorsArr. But they have run into the following error message:
+// Step 3
+// Now that the ReferenceError is resolved, the console is displaying the correct results for a random number between 0 and 9. But CamperBot was not expecting to see decimal numbers like these:
+
 // Example Code
-// Uncaught ReferenceError: math is not defined
-// A ReferenceError is thrown when a non-existent variable is referenced. In this case, it looks like CamperBot is trying to use math but JavaScript doesn't have a math object.
-// Fix CamperBot's error in the math.random() line and open up the console again.
+// 0.015882899879771095
+// 2.114596286197641
+// 6.040964780197666
+// Update the console statement to print a whole number between 0 and 9.
+
+// Remember that you worked with a method in the Role Playing Game that rounds a number down to the nearest whole number.
 
 const darkColorsArr = [
   "#2C3E50",
@@ -18,6 +22,6 @@ const darkColorsArr = [
   "#800020",
 ];
 function getRandomIndex() {
-  console.log(darkColorsArr.length * Math.random())
+    console.log(Math.floor(darkColorsArr.length * Math.random()))
 }
 getRandomIndex()
